336524a25bbd26f6067823f9cad56b640237c2ed
Jonathan Birge <birge@mit.edu>
2023-12-29 02:37:15 -0500

    exclusions taken from file

--- TODO.md
+++ TODO.md
@@ -1,9 +1,8 @@
 # TODO
 
-- Better colormap
-- Exclusion function should move from hardwired list to loading from external JSON file or CSV or environment variable
 - Unified function to handle searches and paging, called by heatmap and front-end, that returns JSON array?
 - External PHP function to create table header array? May not be neccesary if refactor puts this info in only one unified table generation function.
 - Separate link next to IP for running whois on IP address
 - Search interface should specify fields as parameter
 - Multiple field searches should AND together
+- Make function/object to decide if IP is excluded to abstract out the logic
--- exclusions.php
+++ exclusions.php
@@ -1,8 +1,57 @@
 <?php
 
+$exclusionFileName = "exclusions.json";
+
 function getExcludedIPs() {
-    $excludedIPs = ['173.48.140.140', '172.58.222.184', '167.71.120.76', '159.203.109.206'];
-    return $excludedIPs;
+    global $exclusionFileName;
+    // Read the exclusions file and convert it to a PHP array
+    $exclusions = json_decode(file_get_contents($exclusionFileName), true);
+    
+    // If the exclusions file is empty, return an empty array
+    if ($exclusions == null) {
+        return array();
+    } else {
+        return $exclusions;
+    }
+}
+
+function addExcludedIP($ip) {
+    global $exclusionFileName;
+    // Read the exclusions file and convert it to a PHP array
+    $exclusions = json_decode(file_get_contents($exclusionFileName), true);
+    
+    // If the exclusions file is empty, create an empty array
+    if ($exclusions == null) {
+        $exclusions = array();
+    }
+    
+    // Add the IP to the exclusions array
+    $exclusions[] = $ip;
+    
+    // Write the exclusions array to the exclusions file
+    file_put_contents($exclusionFileName, json_encode($exclusions));
+}
+
+function removeExcludedIP($ip) {
+    global $exclusionFileName;
+    // Read the exclusions file and convert it to a PHP array
+    $exclusions = json_decode(file_get_contents($exclusionFileName), true);
+    
+    // If the exclusions file is empty, return an empty array
+    if ($exclusions == null) {
+        return;
+    }
+    
+    // Find the IP in the exclusions array
+    $index = array_search($ip, $exclusions);
+    
+    // If the IP is found, remove it from the exclusions array
+    if ($index !== false) {
+        unset($exclusions[$index]);
+    }
+    
+    // Write the exclusions array to the exclusions file
+    file_put_contents($exclusionFileName, json_encode($exclusions));
 }
 
 ?>
