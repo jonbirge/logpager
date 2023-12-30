9ade65b4dc891cdbd27d7785f44370d9484128d3
Jonathan Birge <birge@mit.edu>
2023-12-29 19:26:33 -0500

    fix heatmap hour axis

--- heatmap.php
+++ heatmap.php
@@ -10,11 +10,20 @@ $excludedIPs = getExcludedIPs();
 // Log file to read
 $logFilePath = '/access.log';
 
-// Open th e log file for reading
+// Open the log file for reading
 $logFile = fopen($logFilePath, 'r');
 if (!$logFile) {
-    echo "Failed to open log file.";
+    echo "<p>Failed to open log file.</p>";
     return;
+}
+
+// Hour integer to string conversion function
+function hourStr($hour) {
+    if ($hour < 10) {
+        return "0$hour";
+    } else {
+        return "$hour";
+    }
 }
 
 // Initialize an empty array to store the log summary data
@@ -22,44 +31,44 @@ $logSummary = [];
 
 // Read each line of the log file
 while (($line = fgets($logFile)) !== false) {
-    // Extract the elements from the CLF log entry
-    $logEntry = explode(' ', $line);
-    $ipAddress = $logEntry[0];
-
-    // Extract the timestamp from the CLF log entry
-    $timeStamp = $logEntry[3];
-    // Convert the timestamp to a DateTime object
-    $date = DateTime::createFromFormat('[d/M/Y:H:i:s', $timeStamp);
-
-    // Check if the DateTime object was created successfully
-    if ($date !== false) {
-        // Get the day of the year
-        // $dayOfYear = $date->format('z') + 1; // Adding 1 because 'z' starts at 0
-        // Get the date in the format YYYY-MM-DD
-        $dayOfYear = $date->format('Y-m-d');
-        // Get the hour of the day
-        $hour = $date->format('G'); // 24-hour format without leading zeros
-    } else {
-        echo "Invalid timestamp format!";
-        return;
-    }
-
-    if (!isset($logSummary[$dayOfYear][$hour])) {
-        $logSummary[$dayOfYear][$hour] = 0;
-    }
-
     // Skip this log entry if the search term isn't found in $line
     if ($searchTerm !== null && strpos($line, $searchTerm) === false) {
         continue;
     }
+
+    // Extract the elements from the CLF log entry
+    $logEntry = explode(' ', $line);
+    $ipAddress = $logEntry[0];
 
     // Skip this log entry if the IP address is in the excluded list
     if (in_array($ipAddress, $excludedIPs)) {
         continue;
     }
 
+    // Extract the timestamp from the CLF log entry
+    $timeStamp = $logEntry[3];
+
+    // Convert the timestamp to a DateTime object
+    $date = DateTime::createFromFormat('[d/M/Y:H:i:s', $timeStamp);
+
+    // Check if the DateTime object was created successfully
+    if ($date !== false) {
+        // Get the date in the format YYYY-MM-DD
+        $dayOfYear = $date->format('Y-m-d');
+        // Get the hour of the day
+        $hour = $date->format('G'); // 24-hour format without leading zeros
+    } else {
+        echo "<p>Invalid timestamp format encountered: $timeStamp</p>";
+        return;
+    }
+
+    // Initialize the count for the day of the year and hour of the day
+    $hStr = hourStr($hour);
+    if (!isset($logSummary[$dayOfYear][$hStr])) {
+        $logSummary[$dayOfYear][$hStr] = 0;
+    }
     // Increment the count for the day of the year and hour of the day
-    $logSummary[$dayOfYear][$hour]++;
+    $logSummary[$dayOfYear][$hStr]++;
 }
 
 // Close the log file
