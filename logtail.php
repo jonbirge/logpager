5546f2ff2af1a07c5573aa8829171969d59b1096
Jonathan Birge <birge@mit.edu>
2023-12-29 21:17:37 -0500

    logtail uses sed to exclude ips

--- logtail.php
+++ logtail.php
@@ -7,27 +7,33 @@ $linesPerPage = $_GET['n'] ?? 20;
 // Path to the CLF log file
 $logFilePath = '/access.log';
 
-// Function to read the nth page from the end of the file
-function getTailPage($filePath, $linesPerPage, $page = 0) {
-    // compute the first and last line numbers
-    $firstLine = $page * $linesPerPage + 1;
-    $lastLine = $firstLine + ($linesPerPage - 1);
+// IP addresses to exclude from counts
+include 'exclude.php';
+$excludedIPs = getExcludedIPs();
 
-    // use popen to read the file in reverse using fast unix tools
-    $cmd = sprintf('tail -n %d %s | head -n %d', $lastLine, escapeshellarg($filePath), $linesPerPage);
-    $fp = popen($cmd, 'r');
+// compute the first and last line numbers
+$firstLine = $page * $linesPerPage + 1;
+$lastLine = $firstLine + ($linesPerPage - 1);
 
-    // read the lines from the pipe
-    $lines = [];
-    while ($line = fgets($fp)) {
-        $lines[] = $line;
-    }
-
-    return $lines;
+// generate UNIX sed command to remove all lines containing any of the excluded IPs
+$sedCmd = '';
+foreach ($excludedIPs as $ip) {
+    $sedCmd .= "sed '/$ip/d' | ";
 }
 
-// Read the last n lines from the file
-$lastPageLines = getTailPage($logFilePath, $linesPerPage, $page);
+// read the file in reverse using fast unix tools
+$escFilePath = escapeshellarg($logFilePath);
+$tailCmd = "tail -n $lastLine | head -n $linesPerPage";
+
+// put everything together into a single command
+$cmd = "cat $escFilePath | $sedCmd $tailCmd";
+$fp = popen($cmd, 'r');
+
+// read the lines from the pipe
+$lines = [];
+while ($line = fgets($fp)) {
+    $lines[] = $line;
+}
 
 // Make array of CLF log headers: IP Address, Timestamp, Request, Status, Size
 $headers = ['IP Address', 'Timestamp', 'Request', 'Status', 'Size'];
@@ -37,7 +43,7 @@ $logLines = [];
 $logLines[] = $headers;
 
 // Process each line and add to the array
-foreach ($lastPageLines as $line) {
+foreach ($lines as $line) {
     preg_match('/(\S+) \S+ \S+ \[(.+?)\] \"(.*?)\" (\S+) (\S+)/', $line, $matches);
     // Go through each match and add to the array with htmlspecialchars()
     $logLines[] = array_map('htmlspecialchars', array_slice($matches, 1));
