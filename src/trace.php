697b3f70fa08693debd4dc7b10ce47d23d3d68d3
Jonathan Birge <birge@mit.edu>
2024-01-28 14:45:12 -0500

    better fonts

--- src/trace.php
+++ src/trace.php
@@ -10,7 +10,11 @@ header("Pragma: no-cache");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600&family=Roboto+Mono&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="styles.css?version=19">
     <?php
     $target_ip = $_GET['ip'];
     echo "<title>Target intel: $target_ip</title>"
