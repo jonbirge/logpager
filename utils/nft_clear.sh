8894a8f6ce44104a0a76c938a530ee65de132d6d
Jonathan Birge <birge@mit.edu>
2024-05-18 21:12:23 -0400

    ip blocking works for forward and input

--- utils/nft_clear.sh
+++ utils/nft_clear.sh
@@ -3,20 +3,22 @@
 # Set name
 SET_NAME=auto_blocked
 
-# Delete the rule that drops packets from IPs in the set
-echo "Deleting rule to drop packets..."
-nft delete rule inet filter "$SET_NAME" ip saddr @"$SET_NAME" drop || true
-
-# Delete the chain for the auto-blocked IPs/CIDRs
-echo "Deleting chain..."
-nft delete chain inet filter "$SET_NAME" || true
-
-# Delete the table
-echo "Deleting table..."
-nft delete table inet filter || true
+# Delete the rules that drop packets from IPs in the set
+echo "Deleting rules to drop packets..."
+nft delete rule inet filter "${SET_NAME}_input" ip saddr @"$SET_NAME" drop || true
+nft delete rule inet filter "${SET_NAME}_forward" ip saddr @"$SET_NAME" drop || true
 
 # Delete the auto_blocked set
 echo "Deleting set..."
 nft delete set inet filter "$SET_NAME" || true
 
-echo "IP clean-up complete!"
+# Delete the chains for the auto-blocked IPs/CIDRs
+echo "Deleting chains..."
+nft delete chain inet filter "${SET_NAME}_input" || true
+nft delete chain inet filter "${SET_NAME}_forward" || true
+
+# Delete the table
+echo "Deleting table..."
+nft delete table inet filter || true
+
+echo "IP unblocking complete!"
--- utils/nft_update.sh
+++ utils/nft_update.sh
@@ -2,7 +2,7 @@
 
 # Check if a command-line argument was provided
 if [ $# -lt 1 ]; then
-    echo "No arguments provided. Please provide the path to the IP file."
+    echo "No arguments provided. Please provide the path to the IP blacklist file."
     exit 1
 fi
 
@@ -15,13 +15,15 @@ SET_NAME=auto_blocked
 # Define a new ruleset
 nft add table inet filter
 
-# Delete the chain that contains the rule referencing the auto_blocked set, if it exists
-echo "Deleting old chain..."
-nft delete chain inet filter "$SET_NAME" || true
+# Delete the chains that contain the rule referencing the auto_blocked set, if they exist
+echo "Deleting old chains..."
+nft delete chain inet filter "${SET_NAME}_input" || true
+nft delete chain inet filter "${SET_NAME}_forward" || true
 
-# Create a new chain for the auto-blocked IPs/CIDRs
-echo "Creating new chain..."
-nft add chain inet filter "$SET_NAME" { type filter hook input priority -1000\; }
+# Create new chains for the auto-blocked IPs/CIDRs
+echo "Creating new chains..."
+nft add chain inet filter "${SET_NAME}_input" { type filter hook input priority -1000\; }
+nft add chain inet filter "${SET_NAME}_forward" { type filter hook forward priority -1000\; }
 
 # Delete the auto_blocked set if it exists
 nft delete set inet filter "$SET_NAME" || true
@@ -42,8 +44,9 @@ do
     fi
 done < "$IP_FILE"
 
-# Add a rule to drop packets from IPs in the set
-echo "Adding rule to drop packets..."
-nft add rule inet filter "$SET_NAME" ip saddr @"$SET_NAME" drop
+# Add rules to drop packets from IPs in the set
+echo "Adding rules to drop packets..."
+nft add rule inet filter "${SET_NAME}_input" ip saddr @"$SET_NAME" drop
+nft add rule inet filter "${SET_NAME}_forward" ip saddr @"$SET_NAME" drop
 
 echo "IP blocking complete!"
