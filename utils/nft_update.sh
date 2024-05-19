81f048abe9efb7f888c6beb5d185532eb1d27afc
Jonathan Birge <birge@mit.edu>
2024-05-19 15:32:51 -0400

    hide errors in nft_update.sh

--- utils/nft_update.sh
+++ utils/nft_update.sh
@@ -15,18 +15,10 @@ SET_NAME=auto_blocked
 # Define a new ruleset
 nft add table inet filter
 
-# Delete the chains that contain the rule referencing the auto_blocked set, if they exist
-echo "Deleting old chains..."
-nft delete chain inet filter "${SET_NAME}_input" || true
-nft delete chain inet filter "${SET_NAME}_forward" || true
-
 # Create new chains for the auto-blocked IPs/CIDRs
 echo "Creating new chains..."
 nft add chain inet filter "${SET_NAME}_input" { type filter hook input priority -1000\; }
 nft add chain inet filter "${SET_NAME}_forward" { type filter hook forward priority -1000\; }
-
-# Delete the auto_blocked set if it exists
-nft delete set inet filter "$SET_NAME" || true
 
 # Create a new set for the auto-blocked IPs/CIDRs, with the 'flags interval' option
 echo "Creating new set..."
@@ -37,7 +29,7 @@ echo "Reading IP addresses and CIDRs from file..."
 while IFS= read -r ip
 do
     if [[ "$ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(/([0-9]|[1-2][0-9]|3[0-2]))?$ ]]; then
-        nft add element inet filter "$SET_NAME" { "$ip" }
+        nft add element inet filter "$SET_NAME" { "$ip" } 2>/dev/null || true
         # echo "Blocking IP/CIDR: $ip"
     else
         echo "Skipping invalid IP/CIDR: $ip"
