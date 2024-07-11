728da9127afce42fc1336a6c830fbf121b5ad0cf
Jonathan Birge <birge@mit.edu>
2024-07-11 01:03:52 +0000

    handle ip ranges that aren't cidrs

--- src/intel/data.php
+++ src/intel/data.php
@@ -9,27 +9,6 @@ $ipinfo = getIntelData($target_ip);
 // Output the data in JSON format
 echo json_encode($ipinfo);
 
-// function getIntelData($ip) {
-//     // geo lookup
-//     $ipURL = "http://ip-api.com/json/$ip?fields=17563647";
-//     //$ipURL = "https://nyc.birgefuller.com/logs/geo.php?ip=$ip";
-//     $ipinfo = file_get_contents($ipURL);
-//     $ipinfo = json_decode($ipinfo, true);
-//     // strip some of the dumber fields
-//     unset($ipinfo['status']);
-//     unset($ipinfo['timezone']);
-//     unset($ipinfo['query']);
-//     unset($ipinfo['lat']);
-//     unset($ipinfo['lon']);
-//     // cidr lookup
-//     $whois = shell_exec("whois $ip");
-//     // find the CIDR block in the whois output of the form xxx.xxx.xxx.xxx/xx
-//     preg_match('/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2}/', $whois, $cidr);
-//     $ipinfo['cidr'] = $cidr[0];
-
-//     return $ipinfo;
-// }
-
 function getIntelData($ip) {
     // geo lookup
     $ipURL = "http://ip-api.com/json/$ip?fields=17563647";
@@ -58,9 +37,11 @@ function getIntelData($ip) {
             $start_ip = $range[1];
             $end_ip = $range[2];
             $cidr = ipRange2cidr($start_ip, $end_ip);
-            $ipinfo['cidr'] = $cidr;
-        } else {
-            $ipinfo['cidr'] = 'Not found';
+            if ($cidr) {
+                $ipinfo['cidr'] = $cidr;
+            } else {
+                $ipinfo['ip range'] = "$start_ip\u{2192}$end_ip";
+            }
         }
     }
 
@@ -77,8 +58,8 @@ function ipRange2cidr($start_ip, $end_ip) {
     if (is_int($masklen)) {
         return long2ip($start) . "/" . $masklen;
     } else {
-        // If we can't create a clean CIDR, return the range
-        return "$start_ip - $end_ip";
+        // If we can't create a clean CIDR, return null
+        return null;
     }
 }
 
