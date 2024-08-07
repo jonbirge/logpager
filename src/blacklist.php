79e9a2284e0e734fd9590d66d6357074edbd03ae
Jonathan Birge <birge@mit.edu>
2024-08-07 02:06:11 +0000

    add addblacklist.php

--- src/blacklist.php
+++ src/blacklist.php
@@ -1,12 +1,7 @@
 <?php
 
-// Create array with all host environment variables
-$host = getenv('SQL_HOST');
-$user = getenv('SQL_USER');
-$pass = getenv('SQL_PASS');
-$db = getenv('SQL_DB');
-$table = 'ip_blacklist';
-$csv_file = '/blacklist.csv';
+// load environment variables
+include 'blacklistutils.php';
 
 // No caching allowed
 header("Cache-Control: no-cache, no-store, must-revalidate");
@@ -19,60 +14,27 @@ if ($conn->connect_error) {
 
 // Handle HTTP methods
 switch ($_SERVER['REQUEST_METHOD']) {
-    case 'GET':  // search for info on IP address or list of them
+    case 'GET':  // search for single IP address
         // Set doc type to JSON
         header('Content-Type: application/json');
 
         // Get the IP address from the URL
         $ip = $_GET['ip'];
 
-        if (empty($ip)) {  // Get IP list from body
+        if (empty($ip)) {  // Return everything
             $blacklist = read_sql_recent($conn, $table);
             echo json_encode($blacklist);
-        } else {  // Get single IP from URL
+        } else {  // Search for single IP from URL
             $blacklist = search_blacklist($ip, $conn, $table);
             echo json_encode($blacklist);
         }
         break;
 
-    case 'POST':  // add new IP address to the blacklist
-        // Get the IP address from the query string
-        $ip = $_GET['ip'];
-
-        // Get the optional data from the POST request body
-        $log_type = $_POST['log_type'];
-        $log = $_POST['log'];
-        $timestamp = $_POST['last_seen'];
-
-        // Check to see if IP address is empty
-        if (empty($ip)) {
-            die("no IP address provided!");
-        }
-
-        // Insert $ip and $log (if exists) into the 'blacklist' table
-        if ($log === 'NULL') {
-            $sql = "INSERT INTO $table (cidr, last_seen, log_type) VALUES ('$ip', '$timestamp', '$log_type')";
-        } else {
-            $sql = "INSERT INTO $table (cidr, last_seen, log_type, log_line) VALUES ('$ip', '$timestamp', '$log_type', '$log')";
-        }
-
-        try {
-            $conn->query($sql);
-            if ($conn->error) {
-                throw new Exception("SQL error: " . $conn->error);
-            }
-        } catch (Exception $e) {
-            $conn->close();
-            echo $e->getMessage();
-            die($e->getMessage());
-        }
-
-        // Write to CSV file
-        $blacklist = read_sql_recent($conn, $table);
-        write_csv($blacklist, $csv_file);
-
-        echo $ip . ' added to blacklist';
-        break;
+    // case 'POST':  // search for list of IP addresses
+    //     // Get the ip list the POST request body
+    //     $ips= $_POST['ips'];
+    //     echo json_encode($ips);
+    //     break;
 
     case 'DELETE':  // delete IP address from the blacklist
         // Get the IP address from the URL
@@ -102,83 +64,3 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
 // Close SQL connection
 $conn->close();
-
-
-// ***** Utility Functions *****
-
-function search_blacklist($ip, $conn, $table) {
-    // SQL query
-    $sql = "
-            SELECT * FROM $table 
-            WHERE 
-                cidr = ? 
-                OR (
-                    cidr LIKE '%.%.%.%/%' AND
-                    INET_ATON(?) BETWEEN 
-                    INET_ATON(SUBSTRING_INDEX(cidr, '/', 1)) 
-                    AND 
-                    INET_ATON(SUBSTRING_INDEX(cidr, '/', 1)) + POW(2, 32 - SUBSTRING_INDEX(cidr, '/', -1)) - 1
-                )
-        ";
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $ip, $ip);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    
-    if ($conn->error) {
-        die("SQL error: " . $conn->error);
-    }
-
-    // create an array of the rows returned
-    $blacklist = [];
-    while ($row = $result->fetch_assoc()) {
-        $blacklist[] = $row;
-    }
-
-    return $blacklist;
-}
-
-// function to write all blacklist IPs/CIDRs to a csv file
-function write_csv($blacklist, $csv_file) {
-    $file = fopen($csv_file, 'w');
-    foreach ($blacklist as $cidr) {
-        fputcsv($file, [$cidr]);
-    }
-    fclose($file);
-}
-
-// function to write all ip/cidr values to a yml file suitable for use with traefik's denyip plugin middleware with the following examples format:
-function write_yml($blacklist, $yml_file) {
-    $file = fopen($yml_file, 'w');
-    fwrite($file, "http:\n");
-    fwrite($file, "  middlewares:\n");
-    fwrite($file, "    blacklist:\n");
-    fwrite($file, "      plugin:\n");
-    fwrite($file, "        denyip:\n");
-    fwrite($file, "          ipDenyList:\n");
-    foreach ($blacklist as $cidr) {
-        fwrite($file, "          - $cidr\n");
-    }
-    fclose($file);
-}
-
-// function to read all cidr values from SQL into array
-function read_sql_recent($conn, $table, $days = 90) {
-    $sql = "SELECT * FROM $table WHERE last_seen >= DATE_SUB(CURDATE(), INTERVAL ? DAY)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $days);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    
-    if ($conn->error) {
-        die("SQL error: " . $conn->error);
-    }
-    
-    $blacklist = [];
-    while ($row = $result->fetch_assoc()) {
-        $blacklist[] = $row['cidr'];
-    }
-
-    return $blacklist;
-}
